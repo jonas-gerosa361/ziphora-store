6d4ffb46620fe70854a75c17989414da1b867168
jonas-gerosa361 <ti.jonas361@gmail.com>
2021-02-03 22:23:51 -0300

    Iniciando projeto, aguardando documento com formatações

---
+++ resources/views/components/menu-sobre.blade.php
@@ -0,0 +1,1 @@
+<h2 class="display-2">Display 2</h2>
--- resources/views/templates/external.blade.php
+++ resources/views/templates/external.blade.php
@@ -11,20 +11,32 @@
     <link rel="stylesheet" type="text/css" href="/asset/css/main.css">
     <script src="/asset/js/sweetalert.js"></script>
     <script src="/asset/js/axios.min.js"></script>
-    <title>Suporte DataSafer</title>
+    <title>Ziphora Store</title>
   </head>
   <body>
     <!-- Header -->
-    <nav id="top" class="navbar navbar-expand-lg navbar-light">
+    <nav id="top" class="navbar top-menu navbar-expand-lg navbar-light">
       <a class="navbar-brand" href="/tickets"><img src="/asset/images/Logo_DataSafer.png" width="300em" alt="DATASAFER LOGO"/></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link">Perfumes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Cuidado com a pele</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Sobre</a>
+          </li>
+        </ul>
+      </div>
     </nav>
     <!-- END OF HEADER -->
     <!-- CONTENT -->
     <div class="col-md-12">
-        <hr>
       @yield('content')
     </div>
     <!-- END OF CONTENT -->
